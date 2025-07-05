d6f37e2e010c53f2c9483db049e12b2bb4827eca
Thallentos <thallescmarques@gmail.com>
2025-07-05 07:36:30 -0300

    inicializando o projeto

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+
+import createApp from "./app";
+
+const app = createApp();
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
