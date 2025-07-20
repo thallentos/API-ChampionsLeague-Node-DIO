ee4c2cf3c651d735be7bb305cf2c7fb3e017d2df
Thallentos <thallescmarques@gmail.com>
2025-07-20 17:49:59 -0300

    criando a camada de repositories e enviando a arquitetura do meu projeto

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,14 +1,15 @@
+import * as PlayersRepository from "../repositories/players-respository";
 import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-  const data = { player: "Antony, o GOAT" };
-  let response = null
+  const data = await PlayersRepository.findAllPlayers();
+  let response = null;
 
   if (data) {
     response = await ok(data);
   } else {
     response = await noContent();
   }
-  
+
   return response;
 };
