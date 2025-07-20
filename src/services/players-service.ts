57842f7c29fb4bd8c26d84fc46238e5584afa4b4
Thallentos <thallescmarques@gmail.com>
2025-07-20 18:34:03 -0300

    criando as funções para listar todos os jogadores e outra para listá-los pelo id

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,14 +1,28 @@
 import * as PlayersRepository from "../repositories/players-respository";
-import { noContent, ok } from "../utils/http-helper";
+import * as HttpResponse from "../utils/http-helper";
 
 export const getPlayerService = async () => {
   const data = await PlayersRepository.findAllPlayers();
   let response = null;
 
   if (data) {
-    response = await ok(data);
+    response = await HttpResponse.ok(data);
   } else {
-    response = await noContent();
+    response = await HttpResponse.noContent();
+  }
+
+  return response;
+};
+
+export const getPlayerByIdService = async (id: number) => {
+  //pedir pro repositório de dados
+  const data = await PlayersRepository.findPlayerById(id);
+  let response = null;
+
+  if (data) {
+    response = await HttpResponse.ok(data);
+  } else {
+    response = await HttpResponse.noContent();
   }
 
   return response;
