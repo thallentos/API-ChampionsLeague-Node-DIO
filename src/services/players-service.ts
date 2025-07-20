ac165e6fb6071a33b704892c094822042177331a
Thallentos <thallescmarques@gmail.com>
2025-07-20 16:25:49 -0300

    delegando responsabilidades de regras de negócio para a service

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,3 +1,14 @@
+import { noContent, ok } from "../utils/http-helper";
+
 export const getPlayerService = async () => {
-    return { player: "Antony, o GOAT" };
+  const data = { player: "Antony, o GOAT" };
+  let response = null
+
+  if (data) {
+    response = await ok(data);
+  } else {
+    response = await noContent();
+  }
+  
+  return response;
 };
